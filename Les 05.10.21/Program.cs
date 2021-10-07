78bfa043edc01e0f418800159b07828505a567c9
AkinO <AkinO@PC215-08>
2021-10-07 14:01:30 +0200

    Add project files.

---
+++ Les 05.10.21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_05._10._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
